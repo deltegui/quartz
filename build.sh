76e4ac348e5a61141f32521e429a5b9803bbedd9
deltegui <deltegui@hotmail.com>
2021-07-18 20:59:21 +0200

    updated make to support debug builds for windows

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm ./quartz.zip
 rm -r ./dist
+
 cd ./qcc
 make
 make win64
@@ -7,6 +8,14 @@ cd ..
 mkdir ./dist
 mv ./qcc/quartz ./dist
 mv ./qcc/quartz.exe ./dist
+
+cd ./qcc
+make debug
+make win64-debug
+cd ..
+mv ./qcc/quartz ./dist/quartz-debug
+mv ./qcc/quartz.exe ./dist/quartz-debug.exe
+
 cp -a ./qcc/programs ./dist
 zip -r quartz.zip ./dist
 rm -r ./dist

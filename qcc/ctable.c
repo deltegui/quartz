15e2576a46d5f2462c80e4964da2ec6bcd4c5167
deltegui <deltegui@hotmail.com>
2021-08-31 20:04:25 +0200

    initial ctable implementation

--- qcc/ctable.c
+++ qcc/ctable.c
@@ -4,29 +4,31 @@
 
 #define LOAD_FACTOR 0.7
 
-#define IS_EMPTY(symbol) (((CTableEntry*) symbol)->key.length == 0)
+#define IS_EMPTY(entry) ((entry)->key.length == 0)
 #define SHOULD_GROW(table) (table->size + 1 > table->capacity * LOAD_FACTOR)
+#define S_GROW_CAPACITY(cap) (cap < 8 ? 8 : cap * 2)
 
 #define CTABLE_KEY_EQUALS(first, second)\
     (first).hash == (second).hash &&\
     (first).length == (second).length &&\
-    memcmp((first).str, (second).str, (second).length) == 0
+    memcmp((first).start, (second).start, (second).length) == 0
 
+static void reset_data(CTable* const table, size_t element_size);
 static void grow_symbol_table(CTable* const table);
 static CTableEntry* find(CTable* const table, CTableKey* name);
 
-CTableKey create_ctable_key(const char* str, int length) {
+CTableKey create_ctable_key(const char* start, int length) {
     assert(length != 0);
-    assert(str != NULL);
+    assert(start != NULL);
     CTableKey name = (CTableKey){
-        .str = str,
+        .start = start,
         .length = length,
-        .hash = hash_string(str, length),
+        .hash = hash_string(start, length),
     };
     return name;
 }
 
-void init_ctable(CTable* const table, size_t element_size) {
+static void reset_data(CTable* const table, size_t element_size) {
     table->size = 0;
     table->capacity = 0;
     table->mask = 1;
@@ -34,15 +36,21 @@ void init_ctable(CTable* const table, size_t element_size) {
     table->element_size = element_size;
 }
 
+void init_ctable(CTable* const table, size_t element_size) {
+    reset_data(table, element_size);
+    init_vector(&table->data, sizeof(void*));
+}
+
 void free_ctable(CTable* const table) {
     if (table->capacity == 0) {
         return;
     }
     free(table->entries);
-    init_ctable(table, table->element_size);
+    free_vector(&table->data);
+    reset_data(table, table->element_size);
 }
 
-void* ctable_find(CTable* const table, CTableKey* key) {
+CTableEntry* ctable_find(CTable* const table, CTableKey* key) {
     if (table->capacity == 0) {
         return NULL;
     }
@@ -53,24 +61,45 @@ void* ctable_find(CTable* const table, CTableKey* key) {
     return entry;
 }
 
+
+CTableEntry* ctable_next_add_position(CTable* const table, CTableKey key) {
+    if (SHOULD_GROW(table)) {
+        grow_symbol_table(table);
+    }
+    assert(table->size + 1 < table->capacity);
+
+    CTableEntry* destination = (CTableEntry*) find(table, &key);
+    destination->key = key;
+    table->size++;
+
+    return destination;
+}
+
+/*
 void ctable_set(CTable* const table, CTableKey key, void* value) {
     if (SHOULD_GROW(table)) {
         grow_symbol_table(table);
     }
     assert(table->size + 1 < table->capacity);
+
+    uint32_t vector_pos = vector_next_add_position(&table->data);
+    void** elements = (void**) table->data.elements;
+    elements[vector_pos] = value;
+
     CTableEntry* destination = (CTableEntry*) find(table, &key);
     destination->key = key;
-    destination->value = value;
+    destination->vector_pos = vector_pos;
     table->size++;
 }
+*/
 
 static void grow_symbol_table(CTable* const table) {
-    void* old_entries = table->entries;
+    CTableEntry* old_entries = table->entries;
     int old_capacity = table->capacity;
 
     table->capacity = S_GROW_CAPACITY(old_capacity);
     table->mask = table->capacity - 1;
-    table->entries = malloc(table->element_size * table->capacity);
+    table->entries = (CTableEntry*) malloc(sizeof(CTableEntry) * table->capacity);
 
     for (int i = 0; i < table->capacity; i++) {
         CTableEntry* entry = (CTableEntry*) &table->entries[i];
--- qcc/ctable.h
+++ qcc/ctable.h
@@ -5,17 +5,23 @@
 #include "vector.h"
 
 typedef struct {
-    const char* str;
+    const char* start;
     int length;
     uint32_t hash;
 } CTableKey;
 
-CTableKey create_ctable_key(const char* str, int length);
+CTableKey create_ctable_key(const char* start, int length);
 
 typedef struct {
-    Vector keys; // Vector<CTableKey>
-    Vector entries; // Vector <void*>
+    CTableKey key;
+    int vector_pos;
+} CTableEntry;
+
+typedef struct {
+    Vector data; // Vector <void*>
     size_t element_size;
+
+    CTableEntry* entries;
     int size;
     int capacity;
     int mask;
@@ -23,7 +29,27 @@ typedef struct {
 
 void init_ctable(CTable* const table, size_t element_size);
 void free_ctable(CTable* const table);
-void* ctable_find(CTable* const table, CTableKey* key);
-void ctable_set(CTable* const table, CTableKey key, void* value);
+CTableEntry* ctable_find(CTable* const table, CTableKey* key);
+CTableEntry* ctable_next_add_position(CTable* const table, CTableKey key);
+
+#define CTABLE_FOREACH(ctable, type, ...) do {\
+    for (int i = 0; i < (ctable)->data.size; i++) {\
+        type* current = (type*) (ctable)->data.elements[i];\
+        __VA_ARGS__\
+    }\
+} while(false)
+
+#define CTABLE_SET(table, key, value, type) do {\
+    CTableEntry* destination = ctable_next_add_position((table), key);\
+    uint32_t vector_pos = vector_next_add_position(&(table)->data);\
+    type* elements = (type*) (table)->data.elements;\
+    elements[vector_pos] = value;\
+    destination->vector_pos = vector_pos;\
+} while(false)
+
+#define CTABLE_ENTRY_RESOLVE_AS(table, entry, dst, type) do {\
+    type* elements = (type*) (table)->data.elements;\
+    *(dst) = elements[entry->vector_pos];\
+} while(false)
 
 #endif

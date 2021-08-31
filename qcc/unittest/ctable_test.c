21330a70ed42f48f98c44c3788841b19ef2f3a43
deltegui <deltegui@hotmail.com>
2021-08-31 19:59:53 +0200

    initial ctable implementation

--- qcc/unittest/ctable_test.c
+++ qcc/unittest/ctable_test.c
@@ -9,48 +9,56 @@
     free_ctable(&table);\
 } while(false)
 
-struct CharEntry {
-    CTableEntry;
-    char* start;
-    int length;
-};
+#define CTABLE_SET_STR(table, key, value) CTABLE_SET(table, key, value, char*)
+#define CTABLE_ENTRY_RESOLVE_STR(table, entry, dst) CTABLE_ENTRY_RESOLVE_AS(table, entry, dst, char*)
 
 void ctable_can_insert_and_search_for_elements() {
     CTableKey a_key = create_ctable_key("a", 1);
     CTableKey b_key = create_ctable_key("b", 1);
 
     CTABLE_TEST(char*, {
-        ctable_set(&table, a_key, "hola soy a");
-        ctable_set(&table, b_key, "hola soy b");
+        CTABLE_SET_STR(&table, a_key, "hola soy a");
+        CTABLE_SET_STR(&table, b_key, "hola soy b");
 
         CTableEntry* a = ctable_find(&table, &a_key);
-        assert_true(memcmp(a->value, "hola soy a", 10) == 0);
+        char* a_str;
+        CTABLE_ENTRY_RESOLVE_STR(&table, a, &a_str);
+        assert_true(memcmp(a_str, "hola soy a", 10) == 0);
 
         CTableEntry* b = ctable_find(&table, &b_key);
-        assert_true(memcmp(b->value, "hola soy b", 10) == 0);
+        char* b_str;
+        CTABLE_ENTRY_RESOLVE_STR(&table, b, &b_str);
+        assert_true(memcmp(b_str, "hola soy b", 10) == 0);
     });
 }
 
-/*void ctable_can_insert_values() {
+#define CTABLE_SET_INT(table, key, value) CTABLE_SET(table, key, value, int)
+#define CTABLE_ENTRY_RESOLVE_INT(table, entry, dst) CTABLE_ENTRY_RESOLVE_AS(table, entry, dst, int)
+
+void ctable_can_insert_values() {
     CTableKey a_key = create_ctable_key("a", 1);
     CTableKey b_key = create_ctable_key("b", 1);
 
     CTABLE_TEST(int, {
-        ctable_set(&table, a_key, 55);
-        ctable_set(&table, b_key, 69);
+        CTABLE_SET_INT(&table, a_key, 55);
+        CTABLE_SET_INT(&table, b_key, 69);
 
         CTableEntry* a = ctable_find(&table, &a_key);
-        assert_true(a->value == 55);
+        int a_int;
+        CTABLE_ENTRY_RESOLVE_INT(&table, a, &a_int);
+        assert_true(a_int == 55);
 
         CTableEntry* b = ctable_find(&table, &b_key);
-        assert_true(b->value == 69);
+        int b_int;
+        CTABLE_ENTRY_RESOLVE_INT(&table, b, &b_int);
+        assert_true(b_int == 69);
     });
-}*/
+}
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(ctable_can_insert_and_search_for_elements)
-        //cmocka_unit_test(ctable_can_insert_values),
+        cmocka_unit_test(ctable_can_insert_and_search_for_elements),
+        cmocka_unit_test(ctable_can_insert_values)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
